33df7bd2ed6522f7cce400198fa1f239f3c0e058
mope10 <moeprashid1@gmail.com>
2018-12-06 20:34:12 +0500

    indentated file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AppRoutingModule} from './app-routing';
-import { ReactiveFormsModule ,FormsModule }   from '@angular/forms';
+import { AppRoutingModule } from './app-routing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
-import { AuthserviceService} from '../services/auth/authservice.service'
-=======
-import {NgxPaginationModule} from 'ngx-pagination';
->>>>>>> 34e6afc296003eaa26cfab238648e32d547d255f
+import { AuthserviceService } from '../services/auth/authservice.service'
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/pages/landing-page/landing-page.component';
@@ -53,12 +50,7 @@ import { UserProductsComponent } from './components/user-dashboard/user-products
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-<<<<<<< HEAD
-    
-
-=======
     NgxPaginationModule
->>>>>>> 34e6afc296003eaa26cfab238648e32d547d255f
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pages/store/store.component.ts
+++ src/app/components/pages/store/store.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { ConditionalExpr } from '@angular/compiler';
-=======
-import {NgxPaginationModule} from 'ngx-pagination'
->>>>>>> 34e6afc296003eaa26cfab238648e32d547d255f
+import { NgxPaginationModule } from 'ngx-pagination'
 
 @Component({
   selector: 'app-store',
@@ -11,65 +8,62 @@ import {NgxPaginationModule} from 'ngx-pagination'
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
-<<<<<<< HEAD
   category = "All";
-=======
   data = [
-    {  
-      id:"1", 
-      name:"HTC", 
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display",
+    {
+      id: "1",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"2",
-      name:"HTC",
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display", 
+      id: "2",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"3",
-      name:"HTC",
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display", 
+      id: "3",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"4", 
-      name:"HTC", 
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display", 
+      id: "4",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"5", 
-      name:"HTC", 
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display", 
+      id: "5",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"6", 
-      name:"HTC", 
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6' Display", 
+      id: "6",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6' Display",
       price: "5000"
     },
     {
-      id:"7", 
-      name:"HTC", 
-      src:"https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg", 
-      detail:"FHD 6's Display", 
+      id: "7",
+      name: "HTC",
+      src: "https://cdn2.gsmarena.com/vv/bigpic/htc-desire-650.jpg",
+      detail: "FHD 6's Display",
       price: "5000"
     }
   ];
->>>>>>> 34e6afc296003eaa26cfab238648e32d547d255f
   constructor() { }
 
   ngOnInit() {
-    
+
   }
   search(query) {
     this.category = query;
