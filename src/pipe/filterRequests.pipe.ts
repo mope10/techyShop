d2fcaa06cba8bd741c746d3b2d6f8e6a3c385d9f
mope10 <moeprashid1@gmail.com>
2018-12-15 23:16:00 +0500

    filterRequest

--- src/pipe/filterRequests.pipe.ts
+++ src/pipe/filterRequests.pipe.ts
@@ -5,10 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipeRequests implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
     if (!items) return [];
-    if (!searchText) return items;
     return items.filter(it => {
       if(it.shopPrivilages == searchText)
-        return it.toLowerCase().includes("");
+        return it;
     });
   }
 }
