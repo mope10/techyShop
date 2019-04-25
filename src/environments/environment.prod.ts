d71ab2f9b48f7f292ad208a21ebef2439d85ad54
mope10 <moeprashid1@gmail.com>
2019-04-25 20:22:41 +0500

    cors 8080

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false
 };
-// export const environment_url = 'http://localhost:8080';
-export const environment_url = '/';
+export const environment_url = 'http://localhost:8080';
+// export const environment_url = '/';
