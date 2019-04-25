6a5c1bc7a4251a7092094c90628a8c3ad73b0aa6
mope10 <moeprashid1@gmail.com>
2019-04-25 20:37:07 +0500

    is this the prob

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const environment_url = "https://techshops.herokuapp.com/"
+export const environment_url = "/"
 
 /*
  * For easier debugging in development mode, you can import the following file
