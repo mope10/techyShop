fb4a4d4592bb6b49a5dc56665c7141d5950ff019
mope10 <moeprashid1@gmail.com>
2019-04-25 20:12:31 +0500

    cros sad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false
 };
-export const environment_url = 'http://localhost:8000';
+// export const environment_url = 'http://localhost:8080';
+export const environment_url = '/';
